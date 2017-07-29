fb56edf482888ecec668d3876775affd901da394
Alberto Salieto <alberto.salieto@starloopstudios.com>
2017-07-29 16:53:33 +0200

    Movement Controller.

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 1.0f;
+    
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        if (moveHorizontal != 0 || moveVertical != 0)
+        {
+            Vector3 move = new Vector3(moveHorizontal, 0, moveVertical);
+            transform.position += move * _speed * Time.deltaTime;
+        }
+    }
+}
