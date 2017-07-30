16f1e214c4c5987b7db3d6e2c84c562075ceecfe
Alexandru Borsevici <email.alexander@gmail.com>
2017-07-30 12:54:04 +0200

    Update props Implement prop scaler Import terrain

---
+++ Assets/Scripts/Environment/PropScaler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PropScaler : MonoBehaviour
+{
+    [SerializeField]
+    Transform visualObject;
+
+    [SerializeField]
+    float offset;
+
+    [SerializeField]
+    [Range(0, 20)]
+    float scale;
+
+    private void OnValidate()
+    {
+        var position = visualObject.transform.position;
+        visualObject.transform.position = new Vector3(position.x, scale * offset, (scale * offset) - offset);
+        visualObject.transform.localScale = Vector3.one * scale;
+    }
+
+}
