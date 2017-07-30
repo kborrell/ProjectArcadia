2969330cf63ee6d282bf96029191599daa354250
Alberto Martínez <albertomartinezcorbalan@gmail.com>
2017-07-30 11:51:17 +0200

    Added a method to retrieve character facing direction

--- Assets/Scripts/Characters/CharacterMovement.cs
+++ Assets/Scripts/Characters/CharacterMovement.cs
@@ -31,15 +31,19 @@ public class CharacterMovement : MonoBehaviour {
 
             if (moveHorizontal != 0 || moveVertical != 0)
             {
-                Vector3 move = new Vector3(moveHorizontal, 0, moveVertical);
-                transform.position += move * m_speed * Time.deltaTime;
+                m_facingDirection = new Vector3(moveHorizontal, 0, moveVertical);
+                transform.position += m_facingDirection * m_speed * Time.deltaTime;
             }
         }
     }
 
     public float GetSpeed() { return m_speed; }
-
+    public Vector3 GetFacingDirection()
+    {
+        return m_facingDirection;
+    }
     [SerializeField] private float m_speed = 1.0f;
     [SerializeField] private bool m_invertedMovement = false;
+    private Vector3 m_facingDirection;
     private bool m_movementEnabled = true;
 }
