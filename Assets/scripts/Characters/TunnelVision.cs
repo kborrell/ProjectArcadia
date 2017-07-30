81654e22903893c34a661215ea96fbacebfd0447
Miguel Merayo <pelaik@gmail.com>
2017-07-30 14:41:05 +0200

    implement the tunnel mask

--- Assets/scripts/Characters/TunnelVision.cs
+++ Assets/scripts/Characters/TunnelVision.cs
@@ -4,20 +4,12 @@ using UnityEngine;
 
 public class TunnelVision : CharacterVision {
 
-    [SerializeField] private float m_tunnelAngle;
-
     // Use this for initialization
     public override void Init(Light light)
     {
-//		light.type = LightType.Point;
-//
-//		light.type = LightType.Spot;
-//		light.range = GetVisionRange();
-//		light.spotAngle = m_tunnelAngle;
-//		light.transform.localPosition = new Vector3(-0.1f, -2.4f, -2.9f);
-//		light.transform.localEulerAngles = new Vector3(-50, 0, 0);
-        	
-		DefaultVision:Init (light);
+		light.type = LightType.Point;
+		light.range = GetVisionRange();
+		light.transform.localPosition = new Vector3(-0.1f, 4.2f, -2.8f);
 
 		SetTunnelMaskEnabled (true);
     }
@@ -27,8 +19,14 @@ public class TunnelVision : CharacterVision {
 		
 		Vector3 direction = CharactersManager.Instance.getPlayerController ().controlledCharacter.m_movementComponent.GetFacingDirection ();
 
-		float angle = Vector3.Angle (Vector3.up, direction);
+		float angle = Vector3.Angle (Vector3.forward, direction);
 
-		GetRect().transform.rotation = Quaternion.EulerAngles(new Vector3(0,0,angle));
+		if (direction.x > 0) 
+		{
+			angle *= -1;
+		}
+
+
+		GetRect().transform.rotation = Quaternion.Euler(new Vector3(0,0,angle));
 	}
 }
