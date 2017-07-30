b0bd4f8315055c1c7459ab2bb49e6d7b378f8b16
Joel Cemeli <joelcemeli94@gmail.com>
2017-07-30 16:34:24 +0200

    Camera show the future position of the character

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -17,9 +17,14 @@ public class CameraController : SingletonMonoBehaviour<CameraController>
     [SerializeField]
     private float zoom = 15.0f;
 
+    private Vector2 m_forwardOffset;
+
+
     private Vector2 cameraCenter;
     private Vector3 cameraPosition;
     private Camera orthCamera;
+
+    private bool something = false;
 
     public void Initialize()
     {
@@ -38,6 +43,18 @@ public class CameraController : SingletonMonoBehaviour<CameraController>
             var playerPosition = playerToFollow.position;
             var transformRotationEulerAngles = transform.rotation.eulerAngles.x;
 
+            Vector3 facingVector = Vector3.zero;
+
+            CharacterMovement charMovement = playerToFollow.GetComponent<CharacterMovement>();
+
+            if(charMovement != null)
+            {
+                facingVector = charMovement.GetFacingDirection();
+                float speed = charMovement.GetSpeed();
+                facingVector *= speed/2;
+            }
+
+
             cameraCenter = new Vector2(playerPosition.x, playerPosition.z);
             cameraPosition = new Vector3((cameraCenter.x),
                                          elevationY * Mathf.Sin(Mathf.Deg2Rad * (transformRotationEulerAngles)) + 1.0f, //Deal
@@ -45,7 +62,7 @@ public class CameraController : SingletonMonoBehaviour<CameraController>
                                         );
 
             // Smooth movement
-            transform.position = Vector3.Lerp(transform.position, cameraPosition, cameraSpeed);
+            transform.position = Vector3.Lerp(transform.position, cameraPosition + facingVector, cameraSpeed);
 
             orthCamera.orthographicSize = zoom;
         }
