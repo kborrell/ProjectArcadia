4e286ba8860dbbce3c4f1295df9963b5a47e13de
David Martinez <david.martinez.martin.1995@gmail.com>
2017-07-30 13:27:11 +0200

    Added TeleportManager Particles v1

--- Assets/Scripts/TeleportManager.cs
+++ Assets/Scripts/TeleportManager.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.EventSystems;
 using UnityEngine;
@@ -14,7 +14,6 @@ public class TeleportManager : SingletonMonoBehaviour<TeleportManager>
 
     private Character m_previousCharacter;
 	private Character m_objetiveCharacter;
-    private GameObject m_previousParticles;
     private GameObject m_particles;
 
 	public void Initialize()
@@ -26,12 +25,14 @@ public class TeleportManager : SingletonMonoBehaviour<TeleportManager>
 	{
 		if (getCurrentCharacter() != character)
 		{
-            m_particles = GameObject.Instantiate(m_soulParticle, getCurrentCharacter().transform.position, Quaternion.Euler(new Vector3(0.0f, 180.0f, 0.0f)));
-
-            Vector2 v1 = new Vector2(getCurrentCharacter().transform.position.x, getCurrentCharacter().transform.position.z);
-            Vector2 v2 = new Vector2(character.transform.position.x, character.transform.position.z);
+            Vector2 v1 = new Vector2(getCurrentCharacter().transform.right.x, getCurrentCharacter().transform.right.z);
+            Vector2 v2 = new Vector2(character.transform.position.x, character.transform.position.z) - new Vector2(getCurrentCharacter().transform.position.x, getCurrentCharacter().transform.position.z);
             float angle = Vector2.Angle(v1, v2);
-            Debug.Log(angle);
+            if (character.transform.position.z > getCurrentCharacter().transform.position.z)
+                angle *= -1;
+
+			m_particles = GameObject.Instantiate(m_soulParticle, getCurrentCharacter().transform.position, Quaternion.Euler(new Vector3(0.0f, 180.0f, 0.0f)));
+            m_particles.GetComponent<ParticleSystem>().startRotation = angle * Mathf.Deg2Rad;
 
 			m_objetiveCharacter = character;
             m_previousCharacter = getCurrentCharacter();
@@ -92,16 +93,8 @@ public class TeleportManager : SingletonMonoBehaviour<TeleportManager>
 			m_objetiveCharacter = null;
             m_previousCharacter = null;
 
-            m_previousParticles = m_particles;
 			DestroyImmediate(m_particles.gameObject);
 			m_particles = null;
 		}
-    }
-
-    IEnumerator DestroyParticle()
-    {
-        yield return new WaitForSeconds(0.5f);
-        DestroyImmediate(m_previousParticles.gameObject);
-        m_previousParticles = null;
     }
 }
