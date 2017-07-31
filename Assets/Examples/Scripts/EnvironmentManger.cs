600ebf71b75ca5769feac608409942efdead40d9
MATSUMOTO\Shezard <iontxu.ocasar@starloopstudios.com>
2017-07-31 21:34:16 +0200

    New Environment Particle system and soul improvements

---
+++ Assets/Examples/Scripts/EnvironmentManger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentManger : MonoBehaviour {
+
+    [SerializeField]
+    ParticleSystem  DragonFly;
+
+    private void Awake()
+    {
+        DragonFly.Stop();
+    }
+    void Update () {
+        ParticleSwitch();
+
+    }
+
+    void ParticleSwitch() {
+
+        if (TeleportManager.Instance.enabled)
+        {
+            DragonFly.Play();
+        }
+        else
+        {
+            DragonFly.Stop();
+        }
+    }
+}
